9fa220c3842e5b0f3bb8471510710493ff51561f
fkrriii04 <abdulfikri2406@gmail.com>
2023-11-16 12:57:23 +0700

    hurdle 2

--- TUGAS_HURDLE/hurdle 2.py
+++ TUGAS_HURDLE/hurdle 2.py
@@ -1,1 +1,22 @@
-abc
+#Program untuk membelokkan robot
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+#Program untuk menghadapi obstacle
+def maju():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+
+#Program untuk eksekusi robot dengan if else untuk titik finish yang random
+while not at_goal():
+    if wall_in_front():
+        maju()
+        turn_left()
+    elif front_is_clear():
+        move()
