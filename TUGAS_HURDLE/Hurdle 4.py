218e5b1c749826b3ce976972599d7f7a6c17bcad
Ahmad Diksa Sumadiono <148454304+AhmadDiksa@users.noreply.github.com>
2023-11-16 11:14:38 +0700

    Update Keterangan.py

--- TUGAS_HURDLE/Hurdle 4.py
+++ TUGAS_HURDLE/Hurdle 4.py
@@ -1,13 +1,16 @@
+#Program Untuk Bergerak ke Kanan
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
-
+    
+#Program Ketika Objek mencapai pucuk
 def pucuk():
     turn_right()
     move()
     turn_right()
 
+#program agar objek bergerak mengikuti pattern
 def gerak():
     turn_left()
     while wall_on_right():
@@ -17,6 +20,7 @@ def gerak():
         move()
     turn_left()
 
+#Program agar objek tetap bergerak dan berhenti di goal
 while not at_goal():
     if wall_in_front():
         gerak()
