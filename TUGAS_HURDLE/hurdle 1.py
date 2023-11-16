e400f440696a54231108a0f3f04762816ea609e6
fkrriii04 <abdulfikri2406@gmail.com>
2023-11-16 12:45:31 +0700

    hurdle 1

--- TUGAS_HURDLE/hurdle 1.py
+++ TUGAS_HURDLE/hurdle 1.py
@@ -1,1 +1,32 @@
-abcd
+#Program untuk membelokan robot
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+#Program untuk menghadapi obstacle
+def maju():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+
+#Program robot berjalan dan menghadapi obstacle
+def jalan():
+    move()
+    maju()
+    turn_left()
+
+#Program eksekusi jalannya robot
+def jalan_seterusnya():
+    jalan()
+    jalan()
+    jalan()
+    jalan()
+    jalan()
+    jalan()
+
+#Eksekusi
+jalan_seterusnya()
