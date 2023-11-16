9918cf5c7f09f9644246f5d404eeecbfcc33fd5e
Ahmad Diksa Sumadiono <148454304+AhmadDiksa@users.noreply.github.com>
2023-11-16 11:12:04 +0700

    update code

--- TUGAS_HURDLE/Hurdle 4.py
+++ TUGAS_HURDLE/Hurdle 4.py
@@ -1,1 +1,24 @@
-Hhh
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def pucuk():
+    turn_right()
+    move()
+    turn_right()
+
+def gerak():
+    turn_left()
+    while wall_on_right():
+        move()
+    pucuk()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        gerak()
+    else:
+        move()
